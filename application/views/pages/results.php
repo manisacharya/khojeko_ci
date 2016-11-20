c36102c7e0d06345079c76a26c27fd808f14a37f
Sagar Chamling <sgr.raee@gmail.com>
2016-11-20 14:10:59 +0545

    All url of item from recent views and search are solved

--- application/views/pages/results.php
+++ application/views/pages/results.php
@@ -2,48 +2,46 @@
 <div class="listCon">
     <div id="viewcontrols" data-enhance="false">
         <a class="info"  onMouseOver="this.style.color='#ffffff'" onMouseOut="this.style.color='#ffffff'">Showing 1 - 10 of 100 results of Items</a>
-        <a class="gridview" href="#!"><i class="fa fa-th fa-2x" ></i></a>
-        <a class="listview" href="#!"><i class="fa fa-list fa-2x"></i></a>
+        <a class="gridview" href="#"><i class="fa fa-th fa-2x" ></i></a>
+        <a class="listview" href="#"><i class="fa fa-list fa-2x"></i></a>
         <button value="View More" id="view_btn">View More</button>
     </div>
 
     <ul class="list">
         <?php
         $dateFormat = 'Y-m-d';
-        foreach ($searched_items as $oneitem) {
+        foreach ($searched_items as $oneitem): ?>
+        <a href="<?php echo base_url('details/'.$oneitem->item_id);?>">
+            <li>
+                <div class="col-sm-2" id="image_content">
+                    <img src="<?php echo base_url('public/images/item_images/'.$oneitem->image); ?>" class="img-responsive"/>
+                </div>
+                <div class="col-sm-10" id="info_content">
+                    <section class="list-right">
+                        <span class="price">
+                            <a class="button"><?php echo $oneitem->c_name;?></a><br>
+                            <i class="fa fa-eye" ></i>
+                            <i class="fa fa-clock-o" ></i>
+                            <i class="fa fa-heart" ></i>
+                            <i class="fa fa-comment-o"></i>
+                        </span>
+                    </section>
+                    <section class="list-left">
+                    <span class="title">
+                        <label><?php echo $oneitem->title; ?></label><br />
+                        <label>Rs <?php echo $oneitem->price; ?> (<?php echo $oneitem->item_type; ?>)</label><br>
+                        <span class="address">
+                            <span><?php echo $oneitem->avaibility_address; ?></span>
+                            <span><?php echo date($dateFormat, $oneitem->published_date);?></span>
+                        </span>
+                    </span>
+                        <p><?php echo $oneitem->specs; ?></p>
+                    </section>
 
-            echo '
-                            <li>
-                                <div class="col-sm-2" id="image_content">
-                                    <img src="'.base_url().'public/images/item_images/'.$oneitem->image.'" class="img-responsive"/>
-                                </div>
-                                <div class="col-sm-10" id="info_content">
-                                    <section class="list-right">
-                                <span class="price">
-                                    <a class="button">'.$oneitem->c_name.'>>'.$oneitem->c_name.'</a><br>
-                                    <i class="fa fa-eye" ></i>
-                                    <i class="fa fa-clock-o" ></i>
-                                    <i class="fa fa-heart" ></i>
-                                    <i class="fa fa-comment-o"></i>
-                                </span>
-                                    </section>
-                                    <section class="list-left">
-                                <span class="title">
-                                    <b>Rs '.$oneitem->price.'<font color="red">&nbsp;('.$oneitem->item_type.')</font></b><br>
-                                    <a class="sub" href="!#">'.$oneitem->title.'</a><br>
-                                    <span class="address">
-                                        <span>'.$oneitem->avaibility_address.'</span><span>'.date($dateFormat, $oneitem->published_date).'</span>
-                                    </span>
-                                </span>
-                                        <p>'.$oneitem->specs.'</p>
-    
-                                    </section>
-    
-                                </div>
-                            </li>
-                                ';
-        }
-        ?>
+                </div>
+            </li>
+        </a>
+        <?php endforeach;?>
 
     </ul>
 
@@ -57,24 +55,24 @@
 <div class="grid_view">
     <ul class="grid1" id="grid1">
     <?php foreach($searched_personals as $personal): ?>
-        <a href="#" >  <li>
+        <a href="#">
+            <li>
             <div class="col-sm-2" id="image_content">
                 <img src="<?php echo base_url();?>public/images/user.png" class="img-rounded"/>
             </div>
             <div class="col-sm-10" id="info_content">
-
                 <section class="list-left">
-                            <span class="title1">
-                                <b><?php echo $personal->name?></b><br>
-                                <a class="sub1"><?php echo $personal->city?></a><br>
-                                <span class="name1">
-                                    Personal Account
-                                </span>
-                            </span>
+                    <span class="title1">
+                        <b><?php echo $personal->name?></b><br>
+                        <a class="sub1"><?php echo $personal->city?></a><br>
+                        <span class="name1">
+                            Personal Account
+                        </span>
+                    </span>
                 </section>
-
             </div>
-        </li></a>
+            </li>
+        </a>
     <?php endforeach;?>
     </ul>
 </div>
@@ -85,7 +83,8 @@
 <div class="grid_view">
     <ul class="grid1" id="grid1">
         <?php foreach($searched_dealers as $dealer): ?>
-            <a href="#" >  <li>
+            <a href="#">
+                <li>
                     <div class="col-sm-2" id="image_content">
                         <img src="<?php echo base_url();?>public/images/dealer_logos/<?php echo $dealer->logo;?>" class="img-rounded"/>
                     </div>
@@ -102,7 +101,8 @@
                         </section>
 
                     </div>
-                </li></a>
+                </li>
+            </a>
         <?php endforeach;?>
     </ul>
 </div>
