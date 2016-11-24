9dabde23ec852df948670c7d5e0c6ab5043d9811
sagar chamling <sgr.raee@gmail.com>
2016-11-24 05:02:50 +0000

    routes.php edited //item post $route['adpost'] = "post_ad/post_form"; removed

--- application/config/routes.php
+++ application/config/routes.php
@@ -7,8 +7,6 @@ $route['translate_uri_dashes'] = FALSE;
 
 // INDEX CONTROLLER ROUTE
 $route['home'] = "homepage/home";
-//item post
-$route['adpost'] = "post_ad/post_form";
 
 // PERSONAL SESSION CONTROLLER
 $route['login'] = "user_session/login";
