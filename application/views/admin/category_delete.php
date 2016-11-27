0879c2d18e4ab10dd0c456bf0f421432231d80d0
Sagar Chamling <sgr.raee@gmail.com>
2016-11-27 13:08:38 +0545

    Category-problem fixed - repeated script deleted

--- application/views/admin/category_delete.php
+++ application/views/admin/category_delete.php
@@ -1,22 +1,22 @@
 
     <div class="col-sm-12">
         <?php echo form_open('admin/category_delete');?>
-        <?php echo $message;?>
-        <label for="choose">Choose Category To Delete</label>
+            <?php echo $message;?>
+            <label for="choose">Choose Category To Delete</label>
 
-        <div class="cname category">
-            <?php
-                require_once ('templates/category.php');
-                print_list(0, 0, $categories);
-            ?>
-        </div>
+            <div class="cname category">
+                <?php
+                    require_once ('templates/category.php');
+                    print_list(0, 0, $categories);
+                ?>
+            </div>
 
-        <label>Category To Delete:</label>
-        <div name="c_name" id="display_cname"></div>
-        <textarea name="c_id" id="c_id" hidden="hidden"></textarea></br>
-        <?php echo form_error('c_id')?>
-            	
-        <button class="btn-primary">Delete</button>
+            <label>Category To Delete:</label>
+            <div name="c_name" id="display_cname"></div>
+            <textarea name="c_id" id="c_id" hidden="hidden"></textarea></br>
+            <?php echo form_error('c_id')?>
+
+            <button class="btn-primary">Delete</button>
         <?php echo form_close();?>
     
     </div><!--col-sm-12-->
