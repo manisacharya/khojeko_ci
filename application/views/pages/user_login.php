a2e90d081dd271fab43fd7cf6ecabc2c1321ac94
Ushmita <ushmita.tuladhar@gmail.com>
2016-11-29 13:04:35 +0545

    email for lost password updated

--- application/views/pages/user_login.php
+++ application/views/pages/user_login.php
@@ -1,53 +1,56 @@
 
-                <div class="login">
-                    <?php echo form_open('login'); ?>
-                    <table class="login_table2">
-                        <tr class="login_tr">
-                            <td colspan="5">
-                                <h3>Login</h3>
-                            </td>
-                        <tr>
+            <div>
+                <?php echo $pwd_changed;?>
+            </div>
+            <div class="login">
+                <?php echo form_open('login'); ?>
+                <table class="login_table2">
+                    <tr class="login_tr">
+                        <td colspan="5">
+                            <h3>Login</h3>
+                        </td>
+                    <tr>
 
-                        <tr>
-                            <td>Username</td>
-                            <td>
-                                <input type="text"  class="naya" name="user_name" value="">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2" style="text-align: center; color: #d43f3a">
-                                <?php echo form_error('user_name');?>
-                            </td>
-                        </tr>
+                    <tr>
+                        <td>Username</td>
+                        <td>
+                            <input type="text"  class="naya" name="user_name" value="" placeholder="Email or Mobile number">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align: center; color: #d43f3a">
+                            <?php echo form_error('user_name');?>
+                        </td>
+                    </tr>
 
-                        <tr>
-                            <td>Password</td>
-                            <td>
-                                <input type="Password"  class="naya" name="password">
-                            </td>
-                        </tr>
-                        <tr>
-                            <td colspan="2"  style="text-align: center; color: #d43f3a">
-                                <?php echo form_error('password');?>
-                            </td>
-                        </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td>
+                            <input type="Password"  class="naya" name="password">
+                        </td>
+                    </tr>
+                    <tr>
+                        <td colspan="2"  style="text-align: center; color: #d43f3a">
+                            <?php echo form_error('password');?>
+                        </td>
+                    </tr>
 
-                        <tr>
-                            <td></td>
-                            <td>
-                                <button class="btn btn-primary" type="submit">Login</button>
-                            </td>
-                        </tr>
+                    <tr>
+                        <td></td>
+                        <td>
+                            <button class="btn btn-primary" type="submit">Login</button>
+                        </td>
+                    </tr>
 
-                        <tr>
-                            <td></td>
-                            <td>
-                                <a href="#" class="Lost">Lost my password ?</a>
-                            </td>
-                        </tr>
-                    </table>
-                    <?php echo form_close(); ?>
-                </div>
-            </div><!--end of col-sm-9-->
-        </div><!--end of row-->
-     </div><!--end of container-->
+                    <tr>
+                        <td></td>
+                        <td>
+                            <a href="lost_password" class="Lost">Lost my password ?</a>
+                        </td>
+                    </tr>
+                </table>
+                <?php echo form_close(); ?>
+            </div>
+        </div><!--end of col-sm-9-->
+    </div><!--end of row-->
+ </div><!--end of container-->
