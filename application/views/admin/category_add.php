e620bd282464ca84b50c06a2019ddccfd5d12c35
Sagar Chamling <sgr.raee@gmail.com>
2016-12-14 14:56:13 +0545

    Category set_value() after form validation()

--- application/views/admin/category_add.php
+++ application/views/admin/category_add.php
@@ -1,28 +1,27 @@
 
-    <div class="col-sm-12">
-        <?php echo form_open('admin/category_add');?>
-            <?php echo $message;?>
-            <label for="category">Type Category Name: </label>
-            <input type="text" name="c_name"><br><br>
-            <?php echo form_error('c_name')?>
+<div class="col-sm-12">
+    <?php echo form_open('admin/category_add');?>
+        <?php echo $message;?>
+        <label for="new_category">Type Category Name: </label>
+        <input type="text" name="c_name" id="new_category" value="<?php echo set_value('c_name');?>"><br><br>
+        <?php echo form_error('c_name')?>
 
-            <label for="choose">Choose Parent Category</label>
-            <div class="parent category">
-                <?php
-                    require_once ('templates/category.php');
-                    print_list(0, 0, $categories);
-                ?>
-            </div>
-            <label style="border:1px solid #ff404b; padding:3px;" class="parent_click">
-                <a href="#">Set as parent</a>
-            </label><br />
-            <label>Choosed Parent Category:</label>
-            <div id="display_parent"></div>
-            <textarea name="parent_id" id="parent" hidden="hidden"></textarea></br>
-
-            <button type="submit" class="btn-primary">Add</button>
+        <label for="choose">Choose Parent Category</label>
+        <div class="parent category">
+            <?php
+            require_once ('templates/category.php');
+            print_list(0, 0, $categories);
+            ?>
+        </div>
+        <label style="border:1px solid #ff404b; padding:3px;" class="parent_click">
+            <a href="#">Set as parent</a>
+        </label><br />
+        <label for="display_parent">Choosed Parent Category:</label>
+        <div id="display_parent"></div>
+        <textarea name="parent_id" id="parent" hidden="hidden"></textarea></br>
         <?php echo form_error('parent_id'); ?>
-     </form>
-    
-    </div><!--col-sm-12-->
+        <button type="submit" class="btn-primary">Add</button>
+    <?php echo form_close(); ?>
+
+</div><!--col-sm-12-->
 </section>
