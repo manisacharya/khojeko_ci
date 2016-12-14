c27914061c3eb8e3621f60f42ad6387b73b411f9
Sagar Chamling <sgr.raee@gmail.com>
2016-12-14 17:29:28 +0545

    Recent_view_model.php recent views problem solved

--- application/models/database_models/Recent_view_model.php
+++ application/models/database_models/Recent_view_model.php
@@ -16,9 +16,10 @@ class Recent_view_model extends CI_Model {
             $this->db->order_by('rv_id', 'DESC');
             $this->db->join('items', 'items.item_id=recent_view.item_id');
             $this->db->join('item_img', 'item_img.item_id = items.item_id');
+            $this->db->join('user', 'user.user_id = items.user_id');
             $this->db->where('primary', 1);
             $this->db->where('deleted_date', 0);
-            $this->db->where('items.user_id', $user_id);
+            $this->db->where('recent_view.user_id', $user_id);
             $query = $this->db->get('recent_view', 8); // limit 8
 
             $result = $query->result();
