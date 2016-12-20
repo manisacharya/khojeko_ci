6b08b67e1c56ecaf1aa5980e3c02cf3a12c03274
Sagar Chamling <sgr.raee@gmail.com>
2016-12-20 18:27:06 +0545

    Category Switching Updated

--- public/js/category_data.js
+++ public/js/category_data.js
@@ -9,26 +9,43 @@ function sub_category_items(c_slug) {
         success: function(data){
             if(data){
                 var len = data['sub_categories_list'].length;
-                var header = '<div id="content"><div class="category_display"><h4>Sub Category</h4><div class="subcategory_display" id="category1_display">';
-                var content = '';
-                var footer = '</div></div></div>';
+                var content_header =
+                                '<div id="content">' +
+                                    '<div class="category_display">' +
+                                        '<h4>'+data['category_title']+'</h4>' +
+                                        '<div class="subcategory_display" id="category1_display">';
+                var category_content = '';
+                var items_content =     '</div>' +
+                                    '</div>';
+                var content_footer =
+                                '</div>';
                 if(len > 0){
                     for(var i=0; i<len; i++){
-                        content += '<div class="childs"><div class="heading">'+data['sub_categories_list'][i].c_name+'</div></div>';
+                        category_content += '<div class="childs">' +
+                                        '<div class="heading">'+data['sub_categories_list'][i].c_name+'</div>' +
+                                    '</div>';
                     }
                     if(content != ""){
-                        $('#content').replaceWith(header+content+footer);
+                        $('#content').replaceWith(content_header+category_content+items_content+content_footer);
                     }
                 }
                 else {
-                    content += '<div class="childs"><div class="heading">No Sub Categories</div></div>'
-                    $('#content').replaceWith(header+content+footer);
+                    content +=  '<div class="childs">' +
+                                    '<div class="heading">No Sub Categories</div>' +
+                                '</div>'
+                    $('#content').replaceWith(content_header+category_content+items_content+content_footer);
                 }
             }
         },
         error: function(jqXHR, textStatus, errorThrown) {
-            $('#error_content').replaceWith('<div id="error_content"><div class="alert alert-danger alert-dismissable fade in">Something went wrong. Please try again in a moment.' +
-                '<button type="button" class="close" data-dismiss="alert" aria-label="Close">x</button></div></div>');
+            var html =  '<div id="error_content">' +
+                            '<div class="alert alert-danger alert-dismissable fade in">Something went wrong. Please try again in a moment.' +
+                                '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                                    '<span class="glyphicon glyphicon-remove"></span>' +
+                                '</button>' +
+                            '</div>' +
+                        '</div>';
+            $('#error_content').replaceWith(html);
         }
     });
 }
