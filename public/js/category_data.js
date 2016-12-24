46fe0e09afcc322ee2898482e6b7d6f0cddd9201
Sagar Chamling <sgr.raee@gmail.com>
2016-12-25 00:49:28 +0545

    category select with items

--- public/js/category_data.js
+++ public/js/category_data.js
@@ -8,7 +8,13 @@ function sub_category_items(c_slug) {
         dataType:"json", //to parse string into JSON object,
         success: function(data){
             if(data){
-                var len = data['sub_categories_list'].length;
+                var c_array = data['sub_categories_list'];
+                var total_sub_categories = c_array.length;
+
+                var items_array = data['sub_categories_items'];
+                var total_sub_categories_items = items_array.length;
+
+                var main_content = '';
                 var content_header =
                                 '<div id="content">' +
                                     '<div class="category_display">' +
@@ -16,59 +22,68 @@ function sub_category_items(c_slug) {
                                         '<div class="subcategory_display">' +
                                             '<ul class="nav nav-pills nav-justified">';
                 var category_content = '';
+
                 var category_footer =        '</ul>' +
                                         '</div>' +
                                     '</div>';
+
+                var items_content = '';
+
+                if (total_sub_categories > 0) {
+                    for(var i=0; i<total_sub_categories; i++) {
+                        category_content += '<li><a onclick="sub_category_items(\''+c_array[i].c_slug+'\')">'+c_array[i].c_name+'</a></li>';
+                    }
+                }
+                else {
+                        category_content =  '<div class="no-child">No Sub Categories</div>';
+                }
+                main_content = content_header+category_content+category_footer;
+
+
                 var items_header =  '<div class="listCon">'+
                                         '<div id="viewcontrols" data-enhance="false">' +
-                                            '<a class="info">'+data['category_title']+'&nbspItems</a>' +
+                                            '<a class="info">'+data['category_title']+'&nbsp;Items</a>' +
                                         '</div>' +
                                         '<ul class="list">';
-                var items_content = '';
-                items_content +=            '<li>' +
+                var content_footer =    '</ul>' +
+                                    '</div>'+
+                                '</div>';
+
+                if (total_sub_categories_items > 0) {
+                    for(var j=0; j<total_sub_categories_items; j++) {
+                        items_content +=    '<li>' +
                                                 '<div class="col-sm-2" id="image_content">' +
-                                                    '<img src="images/nokia lumia.jpg" class="img-responsive"/>' +
+                                                    '<img src="public/images/item_images/'+items_array[j].image+'" class="img-responsive"/>' +
                                                 '</div>' +
                                                 '<div class="col-sm-10" id="info_content">' +
                                                     '<section class="list-right">' +
                                                         '<span class="price">' +
-                                                            '<a class="button">mobile>>nokia</a><br>' +
-                                                            '<i class="fa fa-eye" ></i>' +
-                                                            '<i class="fa fa-clock-o" ></i>' +
-                                                            '<i class="fa fa-heart" ></i>' +
-                                                            '<i class="fa fa-comment-o"></i>' +
+                                                            '<a class="button">'+items_array[j].category+'</a><br>' +
+                                                            '<a data-toggle="tooltip" data-placement="bottom" title="'+items_array[j].views+'"><span class="glyphicon glyphicon-eye-open"></span></a>&nbsp;' +
+                                                            '<span class="glyphicon glyphicon-time"></span>&nbsp;' +
+                                                            '<span class="glyphicon glyphicon-heart"></span>&nbsp;' +
+                                                            '<a data-toggle="tooltip" data-placement="bottom" title="'+items_array[j].comment_count+'"><span class="glyphicon glyphicon-comment"></span></a>' +
                                                         '</span>' +
                                                     '</section>' +
                                                     '<section class="list-left">' +
                                                         '<span class="title">' +
-                                                        '<b>Rs 12300<font color="red">&nbsp;(new)</font></b><br>' +
-                                                        '<a class="sub" href="!#">Nokia lumia</a><br>' +
-                                                        '<span class="name">' +
-                                                            '<i class="fa fa-check-circle" id="tick"></i><b>&nbsp;Ad by:Bishnu Limbu,</b>' +
-                                                        '</span>' +
+                                                        '<b>Rs '+items_array[j].price+'<font color="red">&nbsp;('+items_array[j].item_type+')</font></b><br>' +
+                                                        '<a class="sub" href="">'+items_array[j].title+'</a><br>' +
                                                         '<span class="address">' +
-                                                            '<span>	Kathmandu,Nepal,</span><span>2072/12/25</span>' +
+                                                            '<span>'+items_array[j].avaibility_address+'</span>&nbsp;<span>2072/12/25</span>' +
                                                         '</span>' +
-                                                        '<p> Product description goes here. Aliquam tincidunt diam varius ultricies auctor. Vivamus faucibus risus tempus, adipiscing justo </p>' +
+                                                        '<p>'+items_array[j].specs+'</p>' +
                                                     '</section>' +
                                                 '</div>' +
                                             '</li>';
-                var items_footer =      '</ul>' +
-                                    '</div>';
-                var content_footer =
-                                '</div>';
-                if (len > 0) {
-                    for(var i=0; i<len; i++) {
-                        category_content += '<li><a onclick="sub_category_items(\''+data['sub_categories_list'][i].c_slug+'\')">'+data['sub_categories_list'][i].c_name+'</a></li>';
-                    }
-                    if (category_content != "") {
-                        $('#content').replaceWith(content_header+category_content+category_footer+items_header+items_content+items_footer+content_footer);
                     }
                 }
+
                 else {
-                    category_content +=  '<div class="no-child">No Sub Categories</div>';
-                    $('#content').replaceWith(content_header+category_content+content_footer);
+                    items_content =  '<div class="no-child">No Items</div>';
                 }
+                main_content += items_header+items_content+content_footer;
+                $('#content').replaceWith(main_content);
             }
         },
         error: function(jqXHR, textStatus, errorThrown) {
