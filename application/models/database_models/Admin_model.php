7bcc69fd9aade10931c94a93ab526755afc5453e
Sagar Chamling <sgr.raee@gmail.com>
2016-12-25 17:58:11 +0545

    Admin Register fixed.

--- application/models/database_models/Admin_model.php
+++ application/models/database_models/Admin_model.php
@@ -15,16 +15,16 @@ class Admin_model extends CI_Model {
     public $avatar;
     public $is_primary;
 
-    public function insert_dealer() {
-        if ($this->db->table_exists('dealer')) {
-            $this->a_id = $this->input->post('a_id');
-            $this->admin_name = $this->input->post('admin_name');
-            $this->address = $this->input->post('address');
-            $this->mob = $this->input->post('mob');
-            $this->avatar = $this->input->post('avatar');
-            $this->is_primary = $this->input->post('is_primary');
-            return TRUE;
+    public function register_admin() {
+        if ($this->db->table_exists('admin')) {
+            $this->a_id         = $this->input->post('a_id');
+            $this->admin_name   = $this->input->post('admin_name');
+            $this->address      = $this->input->post('address');
+            $this->mob          = $this->input->post('mob');
+            $this->avatar       = 1;
+            $this->db->insert('admin', $this);
+            return $this->db->insert_id();
         }
-        return FALSE;
+        return 0;
     }
 }
