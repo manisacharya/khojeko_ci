9c22969d00b2b849c796df828f5cd77a0948959c
manisio <manisacharyaa@gmail.com>
2017-01-19 12:50:40 +0545

    admin item_model function added to Items_model of database_model

--- application/models/database_models/Items_model.php
+++ application/models/database_models/Items_model.php
@@ -252,6 +252,7 @@ class Items_model extends CI_Model {
         return $this->item_id;
     }
 
+    ///
     public function get_ad_name(){
 
         return $this->title;
@@ -390,7 +391,102 @@ class Items_model extends CI_Model {
 //    }
 
 
+    ///admin section
+    public function add_item_admin(){
+        $this->load->model('database_models/user_model');
+        $this->load->model('database_models/document_model');
 
+        if ($this->db->table_exists('items')) {
+
+            $this->title = $this->input->post('ad_title');
+            $this->item_type = $this->input->post('ad_type');
+            $this->quantity = 1;
+
+            if($this->input->post('bought_from') == "Abroad")
+                $this->bought_from = $this->input->post('abroad_country');
+            else
+                $this->bought_from = $this->input->post('bought_from');
+
+            $this->price = $this->input->post('offer');
+
+            $used_for = $this->input->post('used_for_text') . " " . $this->input->post('used_for_time');
+            $this->used_for = $used_for;
+
+            $this->mkt_price = $this->input->post('market_price');
+            $this->verification_number = $this->input->post('document_no');
+
+            $this->is_verified = 0;     //what to do
+            $this->avaibility_address = $this->input->post('address');  //what to do
+
+            $this->published_date = NOW();
+
+            $this->ad_duration = $this->input->post('ad_running_time');
+
+            $this->delivery = $this->input->post('home_delivery');
+
+            if($this->delivery == "yes") {
+                $this->delivery_charge = $this->input->post('delivery_charge');
+            } else {
+                $this->delivery_charge = 0;
+            }
+
+            $this->warranty_period = $this->input->post('warranty');
+
+            $this->sales_status = 1;
+            //$this->xss_invoke('ad_duration', 'ad_running_time');
+
+            $this->sales_status = 1;
+
+            $this->views = 0;
+
+            //what to set visibility?
+            $this->visibility = 1;
+
+            $this->video_url1 = $this->video_explode($this->input->post("video1_url"));
+            $this->video_url2 = $this->video_explode($this->input->post("video2_url"));
+
+            $this->deleted_date = 0;
+
+            $this->c_id = $this->input->post('parent_id');
+
+            $this->c_id = $this->categories_model->get_cid_c_slug($this->input->post('postc_slug'));;
+
+            $this->user_id = $this->user_model->get_user_id();
+
+            $this->is_premium = 0;
+            $this->comment_count = 0;
+            $this->spam_count = 0;
+
+            // if($this->title!=NULL)
+            $this->db->insert('items', $this);
+
+            //echo $this->db->_insert();
+            //$this->db->select('item_id');
+            //$this->db->from('items');
+            $q = $this->db->get_where('items', array('published_date' => $this->published_date));
+            //$this->db->where('published_date', $this->published_date);
+
+            //$query =  $this->db->get();
+            foreach ($q->result() as $row)
+            {
+                $this->item_id = $row->item_id;
+            }
+
+            //$this->add_img($filename_arr);
+            $this->item_spec_model->add_spec();
+            // $this->document_model->add_doc();
+
+            // echo $this->item_id;
+
+        } else {
+            echo show_error('We have encountered some problem. Visit site later.', 500, 'Opps! Something went wrong');
+        }
+    }
+
+    public function get_item_name(){
+
+        return $this->title;
+    }
 
 
 }
